c61e2cb52ba5003cc19a91b0a3ea13b718d9b57a
PeterKahenya <kahenya0@gmail.com>
2025-03-25 10:31:11 +0300

    add react types

---
+++ web/app/lib/types.tsx
@@ -0,0 +1,49 @@
+export type User = {
+    id: string
+    name: string
+    email: string
+    createdAt: string
+    updatedAt?: string
+}
+
+export type UserSignup = {
+    name: string
+    email: string
+    password: string
+    confirmPassword: string
+}
+
+export type Login = {
+    email: string
+    password: string
+}
+
+export type Task = {
+    id: string
+    title: string
+    description: string
+    isComplete: boolean
+    completedAt?: string
+    user_id: string
+    createdAt: string
+    updatedAt?: string
+}
+
+export type TaskCreate = {
+    title: string
+    description: string
+    isComplete: boolean
+}
+
+export type TaskUpdate = {
+    title?: string
+    description?: string
+    isComplete?: boolean
+    completedAt?: string
+}
+
+export type Chat = {
+    role: string
+    context: string
+}
+
