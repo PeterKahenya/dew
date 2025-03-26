35f3a0510ac6b36e3cbb3061e0f447773d35e114
PeterKahenya <kahenya0@gmail.com>
2025-03-26 21:42:10 +0300

    include sidebar in secured endpoint

--- web/app/[user_id]/layout.tsx
+++ web/app/[user_id]/layout.tsx
@@ -1,18 +1,21 @@
 "use client"
+import SideBar from "@/components/SideBar";
 import { useAuth } from "@/contexts/auth";
 import { TasksProvider } from "@/contexts/tasks";
-import { useRouter } from "next/navigation";
-import { use, useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
+import "./layout.css"; // Ensure you have global styles
 
 export default function UserHomeLayout({ children, params }: { children: React.ReactNode, params: Promise<{ user_id: string }> }) {
     const { user_id } = use(params)
     const { user, isLoading } = useAuth()
+    const pathname = usePathname().split("/")[2]
+    const [activeTab, setActiveTab] = useState(pathname)
     const router = useRouter()
 
+
     useEffect(() => {
-        // console.log("Securepage layout useEffect", { user, isLoading })
         if (!isLoading && !user) {
-            // console.log("No user found redirecting to login");
             router.push("/login");
         }
     }, [user, isLoading, router])
@@ -24,6 +27,11 @@ export default function UserHomeLayout({ children, params }: { children: React.R
     }
 
     return <TasksProvider user_id={user_id}>
-        {children}
+        <div className="layout">
+            <SideBar active={activeTab} setActive={(tab) => setActiveTab(tab)} />
+            <main className="content">
+                {children}
+            </main>
+        </div>
     </TasksProvider>
 }
