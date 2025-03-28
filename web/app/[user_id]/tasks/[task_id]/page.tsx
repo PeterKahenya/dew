79f7427f901598a546d8b1552925f44fb815c184
PeterKahenya <kahenya0@gmail.com>
2025-03-28 18:31:20 +0300

    mantinize task detail page

--- web/app/[user_id]/tasks/[task_id]/page.tsx
+++ web/app/[user_id]/tasks/[task_id]/page.tsx
@@ -1,37 +1,36 @@
 "use client"
+import TaskForm from "@/components/TaskForm"
 import { useTasks } from "@/contexts/tasks"
+import { notifications } from "@mantine/notifications"
 import { Task, TaskUpdate } from "lib/types"
-import { use, useState } from "react"
+import { useRouter } from "next/navigation"
+import { use } from "react"
 
 export default function Page({ params, }: { params: Promise<{ task_id: string }> }) {
     const { task_id } = use(params)
-    const { tasks, updateTask } = useTasks()
+    const { tasks, filterTasks, updateTask } = useTasks()
+    const router = useRouter()
     const task = tasks.data.filter((task: Task) => task.id === task_id)[0]
-    const [currentTask, setCurrentTask] = useState<TaskUpdate>({
-        title: task.title,
-        description: task.description,
-        isComplete: task.is_complete,
-        completedAt: null
-    })
 
-    async function handleSave(e: { preventDefault: () => void }) {
-        e.preventDefault()
-        const response = await updateTask(task.id, currentTask)
+    async function handleSave(taskUpdate: TaskUpdate) {
+        const response = await updateTask(task.id, taskUpdate)
         if (response.status === 200) {
-            console.log(response)
+            notifications.show({
+                title: "Hooray!",
+                message: "Updated Successfully!",
+                color: "green",
+            })
+            setInterval(() => router.back(), 1000)
+        } else {
+            notifications.show({
+                title: "Oh No!",
+                message: "Something went wrong with the update",
+                color: "red",
+            })
         }
     }
 
     return <div>
-        <form>
-            <input type="text" value={currentTask.title} onChange={(e) => setCurrentTask({ ...currentTask, title: e.target.value })} placeholder="Task Title" />
-            <textarea value={currentTask.description} onChange={(e) => setCurrentTask({ ...currentTask, description: e.target.value })} placeholder="Task Description" />
-            <input type="checkbox" checked={currentTask.isComplete}
-                onChange={(e) => {
-                    const today = new Date().toISOString()
-                    setCurrentTask({ ...currentTask, isComplete: e.target.checked, completedAt: e.target.checked ? today : null })
-                }} />
-            <button onClick={handleSave}>Save</button>
-        </form>
+        <TaskForm handleSave={handleSave} currentTask={task} />
     </div>
 }
