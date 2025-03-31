687c791611f13a5ada584df74ba5e0d8e8620869
PeterKahenya <kahenya0@gmail.com>
2025-03-31 22:16:12 +0300

    add tasks page (now a tab)

--- android/app/src/main/java/dew/app/mobile/presentation/tasks/TasksScreen.kt
+++ android/app/src/main/java/dew/app/mobile/presentation/tasks/TasksScreen.kt
@@ -1,2 +1,94 @@
 package dew.app.mobile.presentation.tasks
 
+import android.content.Intent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import dew.app.mobile.TaskActivity
+import dew.app.mobile.presentation.tasks.TasksViewModel
+import dew.app.mobile.presentation.today.TaskItem
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TasksScreen(viewModel: TasksViewModel) {
+    val tasksState by viewModel.state.collectAsStateWithLifecycle()
+
+    val context = LocalContext.current
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                context.startActivity(Intent(context, TaskActivity::class.java))
+            }) {
+                Row(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Default.Edit, contentDescription = "Add Task")
+                    Text(text = "Add Task")
+                }
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        topBar = {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth().padding(5.dp).background(Color(0xFFF5F5F5)),
+                placeholder = { Text("Search") },
+                leadingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Default.Menu, contentDescription = "Search")
+                    }
+                },
+                trailingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Default.Search, contentDescription = "Search")
+                    }
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    unfocusedBorderColor = Color.Transparent,
+                    focusedBorderColor = Color.Transparent,
+                    errorBorderColor = Color.Transparent,
+                ),
+            )
+        }
+    ) {
+        if (tasksState.error != null) {
+            Text(text = tasksState.error!!)
+        }
+        Column(modifier = Modifier.padding(it).padding(16.dp)) {
+            if (tasksState.tasks.isNotEmpty()) {
+                LazyColumn {
+                    items(tasksState.tasks) { task ->
+                        TaskItem(task, context)
+                    }
+                }
+            }
+        }
+    }
+}
