89a2e468260afc3a5818c82370823c29bee80aa8
PeterKahenya <kahenya0@gmail.com>
2025-03-31 13:57:14 +0300

    style profile view page

--- android/app/src/main/java/dew/app/mobile/ProfileActivity.kt
+++ android/app/src/main/java/dew/app/mobile/ProfileActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import dagger.hilt.EntryPoint
+import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import dew.app.mobile.presentation.profile.ProfileScreen
+import dew.app.mobile.presentation.profile.ProfileViewModel
 import dew.app.mobile.presentation.ui.theme.DewTheme
 
 @AndroidEntryPoint
@@ -20,9 +17,8 @@ class ProfileActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DewTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(text = "Hello Profile",modifier = Modifier.padding(innerPadding))
-                }
+                val viewModel = hiltViewModel<ProfileViewModel>()
+                ProfileScreen(viewModel)
             }
         }
     }
