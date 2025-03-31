d84d39ab864d15d623d8bd11f5258fdc89bf5315
PeterKahenya <kahenya0@gmail.com>
2025-03-31 13:50:03 +0300

    style signup page2

--- android/app/src/main/java/dew/app/mobile/SignupActivity.kt
+++ android/app/src/main/java/dew/app/mobile/SignupActivity.kt
@@ -4,26 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
-import dew.app.mobile.presentation.login.LoginScreen
-import dew.app.mobile.presentation.login.LoginViewModel
+import dew.app.mobile.presentation.signup.SignupScreen
+import dew.app.mobile.presentation.signup.SignupViewModel
 import dew.app.mobile.presentation.ui.theme.DewTheme
 
 @AndroidEntryPoint
-class TodayActivity : ComponentActivity() {
+class SignupActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DewTheme {
-                Text(text = "Today")
+                val viewModel = hiltViewModel<SignupViewModel>()
+                SignupScreen(viewModel)
             }
         }
     }
